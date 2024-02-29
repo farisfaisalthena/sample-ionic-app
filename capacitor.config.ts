4287d698b4fbd0f3fd3c76c2074518f66f75a6cd
Muhammad Faris <muhammad.farisfaisal03@gmail.com>
2024-03-01 07:48:28 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.farisfaisal.fn.tracker',
+  appName: 'fntrack-mobile-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
